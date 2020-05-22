8848552ac197abf5f083b98fc66fafd22df5b0a4
ikaub <dimanovikov22@gmail.com>
2020-05-23 02:37:19 +0300

    Front-end with no db

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-out.component";
+import Header from "./components/header/header.component";
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Switch>
+                <Route exact={true} path='/' component={HomePage}/>
+                <Route path='/shop' component={ShopPage}/>
+                <Route path='/signin' component={SignInAndSignUpPage} />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
