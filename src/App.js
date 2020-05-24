6073831cc72b802fdbeca2ced9f6b8392d28c81e
ikaub <dimanovikov22@gmail.com>
2020-05-24 04:15:21 +0300

    sign-up complete

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-out.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import {Switch, Route} from 'react-router-dom';
-import {auth} from "./firebase/firebase.utils";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils";
 
 class App extends React.Component{
 
@@ -20,10 +20,19 @@ class App extends React.Component{
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user}, () => {
-                console.log(this.state.currentUser);
-            });
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if(userAuth) {
+                const userRef = createUserProfileDocument(userAuth);
+
+                (await userRef).onSnapshot(snapshot => {
+                    this.setState({currentUser: {
+                        id: snapshot.id,
+                            ...snapshot.data()
+                        }})
+                });
+            } else {
+                this.setState({currentUser: userAuth});
+            }
         });
     }
 
